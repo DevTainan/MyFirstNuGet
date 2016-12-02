5a0c18c0171b18ca965b9fe34c935f78deb3037b
kentseng <kentseng@kingroupsys.com>
2016-12-02 17:12:21 +0800

    Add KLog Class for Demo NuGet.

---
+++ MyFirstNuGet/KLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyFirstNuGet
+{
+    public class KLog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //public void WriteToFile(string message)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
